c34df196c415d3e461a974a0ba5efe42aea3fe80
karsicadmir <>
2022-01-08 17:45:51 +0000

    Initial commit

---
+++ main.lua
@@ -0,0 +1,9 @@
+variable = "Aatrox"
+variable2 = "Zac"
+function funkcija()
+print("My favorite champ is " ..variable)
+print( "My least favorite champ is ".. variable2)
+
+end
+funkcija()
+funkcija()
